8ed9ddb2efa26d28f647ca9ed24e635044d5aaab
ernest <echakhoyan@bluip.com>
2019-11-09 23:45:55 +0400

    useEffect testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import hookActions from "./actions/hooksActions";
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "setSecretWord":
+            return { ...state, secretWord: action.payload };
+        default:
+            throw new Error(`Invalid action type ${action.type}`);
+
+    }
+}
 
 function App() {
-  return (
-      <div data-test="app-component">
-        kkk
-      </div>
-  );
+    const [ state, dispatch ] = React.useReducer(reducer, { secretWord: null });
+
+    const setSecretWord = (secretWord) => {
+        dispatch({ type: "setSecretWord", payload: secretWord });
+    };
+
+    React.useEffect(
+        () => {
+            hookActions.getSecretWord(setSecretWord());
+        },
+        []
+    );
+    return (
+        <div data-test="app-component">
+            kkk
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,37 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import App from "./App";
 import { findByTestAttr } from "./test-utils/testUtil";
+import hooksActions from "./actions/hooksActions";
+
+const mockGetSecretWord = jest.fn();
+
 
 const setup = () => {
-    return shallow(<App />);
+    mockGetSecretWord.mockClear();
+    hooksActions.getSecretWord = mockGetSecretWord;
+    return mount(<App />);
 };
 
 it("renders without crashing", () => {
     const wrapper = setup();
     const component = findByTestAttr(wrapper, "app-component");
     expect(component.length).toBe(1);
+});
+
+
+describe("> getSecretWord call", () => {
+    it("should getSecretWord calls on App mount", () => {
+        setup();
+
+        expect(mockGetSecretWord).toHaveBeenCalled();
+    });
+
+    it("should not call `getSecretWord` after update ", () => {
+        const wrapper = setup();
+
+        mockGetSecretWord.mockClear();
+        wrapper.setProps();
+        expect(mockGetSecretWord).not.toHaveBeenCalled()
+    });
 });
